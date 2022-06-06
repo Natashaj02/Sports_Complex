f464975048252542befb693e677c3ead7fb5e314
Natashaj02 <vincy.natasha@gmail.com>
2022-06-06 22:48:28 +0530

    footer added

---
+++ sports-complex/src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../App.css'
+
+export const Footer = () => {
+      return (
+    <footer className="bg-dark text-light">
+       <p>
+           Copyright &copy; @Manit.ac.in
+       </p>
+    </footer>
+  )
+}
+
+export default Footer
